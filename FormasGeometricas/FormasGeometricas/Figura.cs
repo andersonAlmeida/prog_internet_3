6479beede6a6e9d22ef91ccf3468b1887d6d0194
Anderson Almeida <andi.r.almeida@gmail.com>
2019-08-31 19:04:51 -0300

    exercicio formas geometricas

---
+++ FormasGeometricas/FormasGeometricas/Figura.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FormasGeometricas
+{
+    abstract class Figura
+    {
+        private string Cor { get; set; }
+
+        public virtual void area() { }
+    }
+}
---
+++ FormasGeometricas/FormasGeometricas/Triangulo.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FormasGeometricas
+{
+    class Triangulo : Figura
+    {
+        private double Lado_A{ get; set; }
+        private double Lado_B { get; set; }
+        private double Lado_C { get; set; }
+
+        public override void area()
+        {
+            base.area();
+        }
+    }
+}
