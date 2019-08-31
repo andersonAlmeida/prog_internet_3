e9c57c5ea9b3f4cac034ef9f96289f2b3860f32a
Anderson Almeida <andi.r.almeida@gmail.com>
2019-08-31 20:11:23 -0300

    exercicio banco mundial

---
+++ BancoMundial/BancoMundial/Pessoa.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BancoMundial
+{
+    class Pessoa
+    {
+        public int NumeroDePessoas { get; set; }
+
+        public int Id { get; set; }
+
+        public string Endereco { get; set; }
+
+        public string Tel { get; set; }
+
+        public string Email { get; set; }
+    }
+}
