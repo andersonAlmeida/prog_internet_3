35d77ca2499294726c078a0f6cb5353e162aab1b
Anderson Almeida <andi.r.almeida@gmail.com>
2019-08-31 19:54:45 -0300

    exercicio whatsapp

---
+++ Whatsapp/Whatsapp/MsgFoto.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Whatsapp
+{
+    class MsgFoto : Mensagem
+    {
+        private int tamanho;
+    }
+}
