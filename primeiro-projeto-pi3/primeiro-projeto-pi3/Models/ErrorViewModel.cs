e047760e6bc8224e134c5169ae1d0c774444d5b3
ANDERSON ROSA DE ALMEIDA <630701670@fspoa.br>
2019-09-04 21:02:27 -0300

    projeto inicial asp net core

---
+++ primeiro-projeto-pi3/primeiro-projeto-pi3/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace primeiro_projeto_pi3.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
