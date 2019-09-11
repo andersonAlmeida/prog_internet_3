9e5a2ca92795048b5f5370734fb9f590df002a29
ANDERSON ROSA DE ALMEIDA <630701670@fspoa.br>
2019-09-11 20:58:36 -0300

    update primeiro projeto

---
+++ primeiro-projeto-pi3/primeiro-projeto-pi3/Controllers/DepartmentsController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using primeiro_projeto_pi3.Models;
+
+namespace primeiro_projeto_pi3.Controllers
+{
+    public class DepartmentsController : Controller
+    {
+        public IActionResult Index()
+        {
+            /* Instancia uma lista de Departments */
+            List<Department> list = new List<Department>();
+
+            // setando as properties
+            list.Add(new Department {
+                IdDepartment = 1,
+                Name = "Vendas"
+            });
+
+            list.Add(new Department
+            {
+                IdDepartment = 2,
+                Name = "Esporte"
+            });
+
+            list.Add(new Department
+            {
+                IdDepartment = 3,
+                Name = "Games"
+            });            
+
+            // Passa uma lista de objetos
+            return View(list);
+        }
+    }
+}
---
+++ primeiro-projeto-pi3/primeiro-projeto-pi3/Models/Department.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace primeiro_projeto_pi3.Models
+{
+    public class Department
+    {
+        public int IdDepartment { get; set; }
+        public string Name { get; set; }
+    }
+}
