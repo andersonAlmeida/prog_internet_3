06a6380af648e32f6e20fe6968657c20962223ee
ANDERSON ROSA DE ALMEIDA <630701670@fspoa.br>
2019-09-11 21:39:38 -0300

    new project walmart

---
+++ Walmart/Walmart/Data/WalmartContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace Walmart.Models
+{
+    public class WalmartContext : DbContext
+    {
+        public WalmartContext (DbContextOptions<WalmartContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Walmart.Models.Department> Department { get; set; }
+    }
+}
--- primeiro-projeto-pi3/primeiro-projeto-pi3/Models/Department.cs
+++ primeiro-projeto-pi3/primeiro-projeto-pi3/Models/Department.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,6 +8,7 @@ namespace primeiro_projeto_pi3.Models
 {
     public class Department
     {
+        [Display(Name = "Code")] // cria um label para acessar o atributo na view
         public int IdDepartment { get; set; }
         public string Name { get; set; }
     }
