af86b6d3bd3b07e5d5eb321b8c551d10d2ea0c2c
Anderson Rosa de Almeida <andi.r.almeida@gmail.com>
2019-10-02 21:18:39 -0300

    list sellers

---
+++ Walmart/Walmart/Controllers/SellersController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Walmart.Models;
+
+namespace Walmart.Controllers
+{
+    public class SellersController : Controller
+    {
+        public IActionResult Index()
+        {
+            ICollection<Seller> Sellers = new List<Seller>();
+            Sellers.Add(new Seller(1, "Anderson", "anderson@senac.com.br", new DateTime(2000, 5, 25), 8789.6, new Department()));
+            Sellers.Add(new Seller(1, "Liege", "liege@senac.com.br", new DateTime(2000, 5, 25), 8789.6, new Department()));
+            return View(Sellers);
+        }
+    }
+}
